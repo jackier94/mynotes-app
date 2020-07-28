c5ee8e31abccb99770fe8d3516183b50dfcdcd96
Jaquelin Rocha <jaquir16@gmail.com>
2020-07-27 23:35:34 -0700

    created routes, html, api, typed up ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mynotes-app
+
+This note-taking app lets you create notes by title, save them, and delete notes when you no longer need them.
---
+++ Starter/public/server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+
+var express = require("express");
+var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+// ================================================================================
+// ROUTER
+
+require("/routes/apiRoutes")(app);
+require("/routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
